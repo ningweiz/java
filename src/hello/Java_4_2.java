dd03b6b7d0c0948877d6b1d3af4ff64d44948b86
ningweiz <ningweiz007@sina.com>
2015-03-02 17:18:22 +0800

    项目提交

--- src/hello/Java_4_2.java
+++ src/hello/Java_4_2.java
@@ -7,7 +7,7 @@ public class Java_4_2 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
              Scanner in=new Scanner(System.in);
-//             int n=in.nextInt();
+             int n=in.nextInt();
 //             double sum=0;
 //             int sign=1;
 //             for(int i=1;i<=n;i++)
@@ -15,18 +15,18 @@ public class Java_4_2 {
 //            	 sum+=sign*1.0/i;
 //            	 sign=-sign;
 //             }
-//             System.out.print(sum); 
+//            System.out.print(sum); 
 //             int n=in.nextInt();
-//             int m=in.nextInt();
-//             while(m!=0)
-//             {
-//            	 int r=n%m;
-//            	n=m;
-//            	m=r;            	 
-//             }
-//             System.out.print(n);
-             int a=in.nextInt();
-             System.out.println(a);
+             int m=in.nextInt();
+             while(m!=0)
+             {
+            	 int r=n%m;
+            	n=m;
+            	m=r;            	 
+             }
+             System.out.print(n);
+//             int a=in.nextInt();
+//             System.out.println(a);
              
              
              
