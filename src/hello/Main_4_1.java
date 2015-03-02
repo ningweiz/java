a4b42fcc51e8f19036f0c0f977bd3bbc2199521e
ningweiz <ningweiz007@sina.com>
2015-03-02 17:19:42 +0800

    项目提交

--- src/hello/Main_4_1.java
+++ src/hello/Main_4_1.java
@@ -8,7 +8,7 @@ public class Main_4_1 {
 		// TODO Auto-generated method stub
 		//素数和 我们认为2是第一个素数，3是第二个素数，
 		//5是第三个素数，依次类推。现在，给定两个整数n和m，0<n<=m<=200，
-		//你的程序要计算第n个素数到第m个素数之间所有的素数的和，包括第n个素数和第m个素数。
+		// 你的程序要计算第n个素数到第m个素数之间所有的素数的和，包括第n个素数和第m个素数。
 		Scanner in=new Scanner(System.in);
 		int n=in.nextInt();
 		int m=in.nextInt();
