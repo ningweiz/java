349e974a44325dc39829a9fcc6887ff2fd873b64
ningweiz007 <ningweiz007@sina.com>
2015-04-27 15:47:43 +0800

    0427

--- src/hello/Hello.java
+++ src/hello/Hello.java
@@ -9,19 +9,19 @@ public class Hello {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
           System.out.println("Hello World!");
-//	     Scanner in = new Scanner(System.in);
-//         System.out.println("echo:"+in.nextLine());
-//	    int price = 0;
-//	    final int amout=100; //常量添加final
-//	      int amout =0;
-//	      System.out.println("请输入票面：");
-//	      amout=in.nextInt();
-//	     System.out.print("请输入金额：");
-//	      price=in.nextInt();
-//          System.out.println(amout+"-"+price+"="+(amout-price));
-//          int foot; 
-//          double inch;
-//          //double meter;
+	     Scanner in = new Scanner(System.in);
+         System.out.println("echo:"+in.nextLine());
+	    int price = 0;
+	    final int amout=100; //常量添加final
+	      int amout =0;
+	      System.out.println("请输入票面：");
+	      amout=in.nextInt();
+	     System.out.print("请输入金额：");
+	      price=in.nextInt();
+          System.out.println(amout+"-"+price+"="+(amout-price));
+          int foot; 
+          double inch;
+          //double meter;
 //          Scanner a = new Scanner(System.in);
 //          foot=a.nextInt();
 //          inch=a.nextInt();
